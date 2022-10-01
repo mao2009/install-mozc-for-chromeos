12a636e3a0165395902ccedb75fe2de14f3c76da
loach <39354512+mao2009@users.noreply.github.com>
2022-10-01 15:15:52 +0900

    add file.

---
+++ install-mozc-for-chromeos.sh
@@ -0,0 +1,18 @@
+apt update && upgrade -y
+
+apt install task-japanese locales-all fonts-noto-cjk -y
+
+localectl set-locale LANG=ja_JP.UTF-8
+
+source /etc/default/locale
+
+apt install fcitx-mozc -y
+
+sh -c 'cat <<EOF>>/etc/environment.d/fcitx.conf
+GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS=@im=fcitx
+GDK_BACKEND=x11
+EOF'
+
+echo "/usr/bin/fcitx-autostart" >> ~/.sommelierrc
